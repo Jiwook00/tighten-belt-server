47bfcec4bf3d42c096c8cd206313abf6eea81352
Jiwook00 <jwkim775@gmail.com>
2022-03-09 13:28:09 +0900

    feat: DB 연결 설정 추가

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 const morgan = require("morgan");
 const cors = require("cors");
+const { sequelize } = require("./models");
 
 const app = express();
 const port = process.env.PORT;
@@ -23,6 +24,15 @@ app.get("/", (req, res) => {
 app.use((req, res) => {
   res.status(404).send("Path Not Found");
 });
+
+sequelize
+  .sync({ force: false })
+  .then(() => {
+    console.log("DB 연결 성공");
+  })
+  .catch((err) => {
+    console.log(`DB 연결 오류 : ${err}`);
+  });
 
 app.listen(port, () => {
   console.log(`서버 연결 성공`);
---
+++ config/config.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+module.exports = {
+  development: {
+    username: process.env.DEV_USER,
+    password: process.env.DEV_PASSWORD,
+    database: process.env.DEV_DB,
+    host: process.env.DEV_HOST,
+    dialect: "mysql",
+    timezone: "+09:00",
+    dialectOptions: {
+      dateStrings: true,
+      typeCast: true,
+    },
+  },
+  production: {
+    username: process.env.PROD_USER,
+    password: process.env.PROD_PASSWORD,
+    database: process.env.PROD_DB,
+    host: process.env.PROD_HOST,
+    dialect: "mysql",
+    timezone: "+09:00",
+    dialectOptions: {
+      dateStrings: true,
+      typeCast: true,
+    },
+  },
+};
